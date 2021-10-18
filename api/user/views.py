9a1fb0128aa08b2a027c15c7d5351a6fce38f9fb
nishudon007 <snishant1998@gmail.com>
2021-10-18 23:34:12 +0530

    views for custom user signin

--- api/user/views.py
+++ api/user/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from nis import match
+from rest_framework import viewsets
+from rest_framework.permission import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
+
+
 import random
+import re
+# re is used for running the regular expression
 # Create your views here.
 
 
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+
+
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'Send a post request with valid parameter only'})
+
+    username = request.POST['email']
+    password = request.POST['password']
+
+    # validation part
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", username):
+        return JsonResponse({'error': 'Enter a valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error': 'Password needs to be atleast of 3 characters'})
+
+    UserModel = get_user_model()
