ae5759089c2dc7ef64d69db4c0e40f40ef0e5142
nishudon007 <snishant1998@gmail.com>
2021-10-27 00:02:21 +0530

    custom User code changes

--- api/user/views.py
+++ api/user/views.py
@@ -1,10 +1,12 @@
-from nis import match
+
+from api import user
 from rest_framework import viewsets
-from rest_framework.permission import AllowAny
+from rest_framework.permissions import AllowAny
 from .serializers import UserSerializer
 from .models import CustomUser
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
+# Cross Site Request Forgeries. (CSRF)
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, logout
 
@@ -14,11 +16,14 @@ import re
 # re is used for running the regular expression
 # Create your views here.
 
+# About CSRF - The CSRF middleware and template tag provides easy-to-use protection against Cross Site Request Forgeries. This type of attack occurs when a malicious website contains a link, a form button or some JavaScript that is intended to perform some action on your website, using the credentials of a logged-in user who visits the malicious site in their browser. A related type of attack, ‘login CSRF’, where an attacking site tricks a user’s browser into logging into a site with someone else’s credentials, is also covered.
+
 
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
 
 
+@csrf_exempt
 def signin(request):
     if not request.method == 'POST':
         return JsonResponse({'error': 'Send a post request with valid parameter only'})
@@ -34,3 +39,58 @@ def signin(request):
         return JsonResponse({'error': 'Password needs to be atleast of 3 characters'})
 
     UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(email=username)
+
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(
+                email=username).values().first()
+            usr_dict.pop('password')
+
+            if user.session_token != "0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error': 'Previous Session Exists!!!'})
+
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)  # this particular user is gonna be loggedin
+            return JsonResponse({'token': token, 'user': usr_dict})
+
+        else:
+            return JsonResponse({'error': 'Invalid Password'})
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'Invalid Email'})
+
+
+def signout(request, id):
+    logout(request)
+
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(pk=id)  # pk is another way of writing id
+        user.session_token = "0"
+        user.save()
+
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error': 'Invalid user ID'})
+
+    return JsonResponse({'success': 'Logout Success'})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
+
+    queryset = CustomUser.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
